227f3b8c0988c927f73091a598703e141d98ea36
Paweł Malak <pawel999@icloud.com>
2023-03-01 00:35:32 +0100

    Changed app to Socket Demo

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
 (() => {
-  const appConfig = {
-    name: 'Nameless'
-  };
+  socket = new WebSocket('ws://localhost:5015');
 
-  let isBroadcasting = false;
-
-  const appName = document.getElementById('appName');
-  const nameInput = document.getElementById('nameInput');
-  const dataBtn = document.getElementById('dataBtn');
-
-  nameInput.addEventListener('input', e => {
-    appConfig.name = e.target.value.trim();
-    appName.textContent = `${appConfig.name} Demo App`;
+  socket.addEventListener('open', e => {
+    console.log(e);
+    socket.send('hello server');
   });
 
-  dataBtn.addEventListener('click', () => {
-    isBroadcasting = !isBroadcasting;
-
-    dataBtn.textContent = `${isBroadcasting ? 'Stop' : 'Start'} sending data`;
-
-    nameInput.disabled = isBroadcasting;
-
-    console.log(
-      `${appConfig.name} is now ${isBroadcasting ? 'online' : 'offline'}`
-    );
+  socket.addEventListener('message', e => {
+    console.log(e);
+    console.log(e.data);
   });
 })();
